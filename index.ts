a3c8c2638ef3d19c6e8dcfde03f76fc00bc68c18
Sam Vervaeck <sam@vervaeck.be>
2016-07-09 15:26:25 +0200

    Add more features

--- index.ts
+++ index.ts
@@ -8,10 +8,16 @@ function error(msg: string) {
   console.log(chalk.red(`Error: ${msg}`))
 }
 
-export class REPL {
+function isPromise(val) {
+  return val instanceof Object && typeof val.then === 'function'
+}
 
+export abstract class REPL {
+
+  byeMessage = 'Bye.'
   rl;
   lastError: Error | null = null
+  aliases: { [name: string]: string }
 
   constructor(options?) {
     options = options || {}
@@ -26,22 +32,43 @@ export class REPL {
     })
     const read = async () => {
       this.rl.question(' > ', (answer) => {
-        switch (answer) {
-        case 'stack':
-          if (this.lastError === null)
-            error('No recent errors.')
-          else
-            console.log(this.lastError.stack)
+        try { 
+          const chunks = answer.split(' ')
+              , command = chunks[0]
+          switch (command) {
+          case 'stack':
+            if (this.lastError === null)
+              error('No recent errors.')
+            else
+              console.log(this.lastError.stack)
+            read()
+            break
+          case 'quit':
+            console.log(this.byeMessage)
+            process.exit()
+          case 'alias':
+            const aliasName = chunks[1]
+            if (!aliasName)
+              throw new Error(`must provide an alias name`)
+            if (chunks.length < 3)
+              throw new Error(`must provide a command`)
+            this.aliases[aliasName] = chunks.slice(2).join(' ')
+            console.log(chalk.green(`Alias '${aliasName}' created.`))
+            break
+          default:
+            const res = this.evaluate(answer)
+            if (isPromise(res))
+              res.then(() => read())
+                .catch(e => {
+                  this.lastError = e
+                  error(e.message)
+                  read()
+                })
+          }
+        } catch (e) {
+          this.lastError = e
+          error(e.message)
           read()
-          break
-        default:
-          this.evaluate(answer)
-            .then(() => read())
-            .catch(e => {
-              this.lastError = e
-              error(e.message)
-              read()
-            })
         }
       })
     }
@@ -55,12 +82,6 @@ export class REPL {
   }
 
   abstract evaluate(input: string): Promise<void>
-
-  addREPL(name: string, repl: REPL) {
-    this.addCommand(name, (argv) => {
-      return repl.evaluate(argv)
-    })
-  }
 
 }
 
